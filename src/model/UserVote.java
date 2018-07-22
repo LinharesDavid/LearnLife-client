032e73ee422e00f68751731a38052ba5acdef0a9
Cédric Nanteau <cedric.nanteau@gmail.com>
2018-07-22 12:22:47 +0200

    added the number of votes for a challenge in the challenge table

---
+++ src/model/UserVote.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.util.HashMap;
+
+import static utils.Constants.*;
+
+public class UserVote {
+
+    private String _id;
+    private String user;
+    private String challenge;
+
+
+    public UserVote(String _id, String user, String challenge) {
+        this._id = _id;
+        this.user = user;
+        this.challenge = challenge;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public UserVote set_id(String _id) {
+        this._id = _id;
+        return this;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public UserVote setUser(String user) {
+        this.user = user;
+        return this;
+    }
+
+    public String getChallenge() {
+        return challenge;
+    }
+
+    public UserVote setChallenge(String challenge) {
+        this.challenge = challenge;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "UserVote{" +
+                "_id='" + _id + '\'' +
+                ", user='" + user + '\'' +
+                ", challenge='" + challenge + '\'' +
+                '}';
+    }
+}
